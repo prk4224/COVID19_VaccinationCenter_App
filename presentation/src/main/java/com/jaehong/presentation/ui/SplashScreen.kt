a6591e8116e48a59c0d580ce901f46c590746c8d
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-23 20:30:57 +0900

    Splash Screen 구현

---
+++ presentation/src/main/java/com/jaehong/presentation/ui/SplashScreen.kt
@@ -0,0 +1,46 @@
+package com.jaehong.presentation.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@Composable
+fun SplashScreen(
+    splashViewModel: SplashViewModel = hiltViewModel()
+) {
+    val loadingValue = splashViewModel.loadingValue.collectAsState().value
+    val animationState = splashViewModel.animationState.collectAsState().value
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "$loadingValue %",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(bottom = 50.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .background(Color.LightGray, RoundedCornerShape(10.dp))
+                .width(animationState.dp)
+                .height(20.dp)
+                .align(Alignment.Center),
+        )
+    }
+
+    splashViewModel.startAnimation()
+}
---
+++ presentation/src/main/java/com/jaehong/presentation/ui/SplashViewModel.kt
@@ -0,0 +1,101 @@
+package com.jaehong.presentation.ui
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jaehong.domain.model.CenterItem
+import com.jaehong.domain.model.result.ApiResult
+import com.jaehong.domain.usecase.SplashUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val splashUseCase: SplashUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(false)
+    val uiState = _uiState.asStateFlow()
+
+    private val _animationState = MutableStateFlow(0)
+    val animationState = _animationState.asStateFlow()
+
+    private val _loadingValue = MutableStateFlow(0)
+    val loadingValue = _loadingValue.asStateFlow()
+
+    private lateinit var loading: Job
+
+    init {
+        for (idx in 1..10) {
+            getCenterItems(idx)
+        }
+    }
+
+    fun startAnimation() {
+        initLoadingValue()
+
+        viewModelScope.launch {
+            if (loadingValue.value == 80 && uiState.value.not()) {
+                loading.cancel()
+            }
+            if (loadingValue.value == 100) {
+                loading.cancel()
+            }
+        }
+    }
+
+    private fun initLoadingValue() {
+        loading = viewModelScope.launch {
+            delay(20)
+            _loadingValue.value++
+            _animationState.value += 2
+        }
+    }
+
+    private fun getCenterItems(page: Int) {
+        viewModelScope.launch {
+            splashUseCase.getVaccinationCenters(page)
+                .catch { Log.d("Get Center Items", "${it.message}") }
+                .collect {
+                    when (it) {
+                        is ApiResult.Success -> {
+                            insertCenterItems(it.data, page)
+                        }
+                        is ApiResult.Error -> {
+                            Log.d("Get Center Items", "${it.exception.message}")
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun insertCenterItems(items: List<CenterItem>, page: Int) {
+        viewModelScope.launch {
+            splashUseCase.insertCenterItems(items)
+                .catch { Log.d("Insert Center Items", "${it.message}") }
+                .collect {
+                    if (it && page == 10) {
+                        checkedLoadingValue()
+                    }
+                    if (it.not()) {
+                        Log.d("Insert Center Items", "No.$page Insert Failure")
+                    }
+                }
+        }
+    }
+
+    private fun checkedLoadingValue() {
+        _uiState.value = true
+        if (loading.isCancelled && loadingValue.value == 80) {
+            initLoadingValue()
+        }
+    }
+}
