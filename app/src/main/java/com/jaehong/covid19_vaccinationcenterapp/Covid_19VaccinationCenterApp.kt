04e76d0777999b3f06073f860795531c2a9f20c6
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-23 19:58:03 +0900

    Application Hilt Annotation 추가

--- app/src/main/java/com/jaehong/covid19_vaccinationcenterapp/Covid_19VaccinationCenterApp.kt
+++ app/src/main/java/com/jaehong/covid19_vaccinationcenterapp/Covid_19VaccinationCenterApp.kt
@@ -1,6 +1,8 @@
 package com.jaehong.covid19_vaccinationcenterapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 
+@HiltAndroidApp
 class Covid19VaccinationCenterApp: Application()
