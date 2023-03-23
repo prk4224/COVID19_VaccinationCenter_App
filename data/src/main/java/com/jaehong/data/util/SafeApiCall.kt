0c9bc4531a18420fc40db30c7a43ba84e19f304b
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-23 20:18:37 +0900

    ApiResult 값 반환 로직

---
+++ data/src/main/java/com/jaehong/data/util/SafeApiCall.kt
@@ -0,0 +1,22 @@
+package com.jaehong.data.util
+
+import com.jaehong.domain.model.result.ApiResult
+import retrofit2.Response
+import java.io.IOException
+
+suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>): ApiResult<T> {
+    val response: Response<T>
+
+    try {
+        response = call()
+    } catch (e: IOException) {
+        return ApiResult.Error(IOException(e.message ?: "Internet error runs"))
+    }
+
+    val body = response.body()
+    return if (body != null) {
+        ApiResult.Success(body)
+    } else {
+        ApiResult.Error(IOException(""))
+    }
+}
