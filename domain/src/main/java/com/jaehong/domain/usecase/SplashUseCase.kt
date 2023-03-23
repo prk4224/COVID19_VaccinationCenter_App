a2fe5075960e20ff2f682591d3974b92a44721c4
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-23 20:30:25 +0900

    Local 에 데이터 저장 로직 (UseCase)

--- domain/src/main/java/com/jaehong/domain/usecase/SplashUseCase.kt
+++ domain/src/main/java/com/jaehong/domain/usecase/SplashUseCase.kt
@@ -2,6 +2,7 @@ package com.jaehong.domain.usecase
 
 import com.jaehong.domain.model.CenterItem
 import com.jaehong.domain.model.result.ApiResult
+import com.jaehong.domain.repository.LocalRepository
 import com.jaehong.domain.repository.RemoteRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
@@ -10,12 +11,20 @@ import javax.inject.Inject
 
 class SplashUseCase @Inject constructor(
     private val remoteRepository: RemoteRepository,
+    private val localRepository: LocalRepository
 ) {
 
     suspend fun getVaccinationCenters(
         page: Int,
     ): Flow<ApiResult<List<CenterItem>>> = flow {
         remoteRepository.getVaccinationCenters(page)
+            .collect { emit(it) }
+    }
+
+    suspend fun insertCenterItems(
+        centerItems: List<CenterItem>
+    ): Flow<Boolean> = flow {
+        localRepository.insertCenterItems(centerItems)
             .collect { emit(it) }
     }
 }
