05e8978fc7d20cbcc4986132a9339102052092ee
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-23 20:07:46 +0900

    API 의존성 기능 구현

---
+++ app/src/main/java/com/jaehong/covid19_vaccinationcenterapp/di/NetworkModule.kt
@@ -0,0 +1,70 @@
+package com.jaehong.covid19_vaccinationcenterapp.di
+
+import android.util.Log
+import com.jaehong.covid19_vaccinationcenterapp.Constants.BASE_URL
+import com.jaehong.covid19_vaccinationcenterapp.Constants.SERVICE_KEY
+import com.jaehong.data.remote.service.VaccinationCenterService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor { message ->
+        Log.d("API", message)
+    }.apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiInterceptor(): ApiInterceptor = ApiInterceptor()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        apiInterceptor: ApiInterceptor,
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(apiInterceptor)
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun getClient(
+        client: OkHttpClient,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): VaccinationCenterService {
+        return retrofit.create(VaccinationCenterService::class.java)
+    }
+
+    class ApiInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request().newBuilder().build()
+            return chain.proceed(request)
+        }
+    }
+}
+
