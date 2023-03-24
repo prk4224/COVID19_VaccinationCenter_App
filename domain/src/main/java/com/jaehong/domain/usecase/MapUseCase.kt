b1f5e281e936ebff92ed0cddf19286a64e36bcd2
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-24 22:05:58 +0900

    MapScreen -> UseCase 구현

---
+++ domain/src/main/java/com/jaehong/domain/usecase/MapUseCase.kt
@@ -0,0 +1,19 @@
+package com.jaehong.domain.usecase
+
+import com.jaehong.domain.model.CenterItem
+import com.jaehong.domain.model.Location
+import com.jaehong.domain.repository.LocalRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class MapUseCase @Inject constructor(
+    private val localRepository: LocalRepository,
+) {
+    suspend fun getCenterInfo(): Flow<List<CenterItem>> = flow {
+        localRepository.getCenterInfo().collect {
+            emit(it)
+        }
+    }
+}
