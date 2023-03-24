88ab64dd5d3910829f44198fa7d386acb8ac18fc
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-24 22:03:22 +0900

    Room에서 Center 정보 들고오기 (Repository)

--- data/src/main/java/com/jaehong/data/local/repository/LocalRepositoryImpl.kt
+++ data/src/main/java/com/jaehong/data/local/repository/LocalRepositoryImpl.kt
@@ -3,8 +3,10 @@ package com.jaehong.data.local.repository
 import com.jaehong.data.local.datasource.LocalDataSource
 import com.jaehong.data.util.Mapper.domainFromEntity
 import com.jaehong.domain.model.CenterItem
+import com.jaehong.domain.model.Location
 import com.jaehong.domain.repository.LocalRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
@@ -12,12 +14,18 @@ class LocalRepositoryImpl @Inject constructor(
     private val localDataSource: LocalDataSource
 ): LocalRepository {
 
+    override suspend fun getCenterInfo(): Flow<List<CenterItem>> = flow {
+        localDataSource.getCenterInfo().collect {
+            emit(it.map { item -> item.mappingCenterInfoEntityFromCenterItem() })
+        }
+    }
+
     override suspend fun insertCenterItems(
         centerItems: List<CenterItem>
     ): Flow<Boolean> = flow {
         localDataSource.insertCenterItems(centerItems.map { it.domainFromEntity() })
             .collect {
                 emit(it)
-        }
+            }
     }
 }
--- domain/src/main/java/com/jaehong/domain/repository/LocalRepository.kt
+++ domain/src/main/java/com/jaehong/domain/repository/LocalRepository.kt
@@ -1,9 +1,12 @@
 package com.jaehong.domain.repository
 
 import com.jaehong.domain.model.CenterItem
+import com.jaehong.domain.model.Location
 import kotlinx.coroutines.flow.Flow
 
 interface LocalRepository {
+
+    suspend fun getCenterInfo(): Flow<List<CenterItem>>
 
     suspend fun insertCenterItems(centerItems: List<CenterItem>): Flow<Boolean>
 }
