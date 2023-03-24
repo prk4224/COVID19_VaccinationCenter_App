1a6acaabd98faa0a2aba578f583a3368f2a07c94
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-24 22:07:37 +0900

    권한 Array 구현

---
+++ presentation/src/main/java/com/jaehong/presentation/util/ArrayConstants.kt
@@ -0,0 +1,10 @@
+package com.jaehong.presentation.util
+
+import android.Manifest
+
+object ArrayConstants {
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+}
