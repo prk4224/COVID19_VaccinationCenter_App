32730c83d2d2fedec0366b949c08dec06017392e
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-24 22:02:00 +0900

    Entity Mapper 구현

--- data/src/main/java/com/jaehong/data/local/entity/CenterInfoEntity.kt
+++ data/src/main/java/com/jaehong/data/local/entity/CenterInfoEntity.kt
@@ -2,6 +2,7 @@ package com.jaehong.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.jaehong.domain.model.CenterItem
 
 @Entity
 data class CenterInfoEntity (
@@ -12,4 +13,18 @@ data class CenterInfoEntity (
     val phoneNumber: String,
     val updatedAt: String,
     val centerType: String,
-)
+) {
+    fun mappingCenterInfoEntityFromCenterItem(): CenterItem {
+        return CenterItem(
+            id = this.id,
+            address = this.address,
+            centerName = this.centerName,
+            facilityName = this.facilityName,
+            phoneNumber = this.phoneNumber,
+            updatedAt = this.updatedAt,
+            lat = this.lat,
+            lng = this.lng,
+            centerType = this.centerType,
+        )
+    }
+}
